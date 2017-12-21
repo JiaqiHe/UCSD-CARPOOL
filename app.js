f28e9335b37341ae1cae275d9bbb0f3881ef6567
Jiaqi He <jih417@eng.ucsd.edu>
2017-12-21 21:21:10 +0000

    add mlab

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var nodemailer    = require("nodemailer");
 const sgMail      = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-mongoose.connect("mongodb://localhost/carpool", {useMongoClient: true});
+// mongoose.connect("mongodb://localhost/carpool", {useMongoClient: true});
+mongoose.connect("mongodb://JiaqiHe:1735794a75b@ds163156.mlab.com:63156/ucsdcarpool");
+
 
 var Post          = require("./models/post");
 var Message       = require("./models/message");
